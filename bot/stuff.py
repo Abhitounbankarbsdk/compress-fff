ca3e7ca263135f2920050ce81508ffc3322b5fbd
Anshukumarsharma <88080930+Anshukumarsharma@users.noreply.github.com>
2021-09-13 14:59:01 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MOVIES GROUP", url="https://t.me/GorgeousAndGloreous"),
+                Button.url("DEVELOPER", url="t.me/Anshu888o"),
             ],
         ],
     )
@@ -59,8 +59,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MOVIES GROUP", url="https://t.me/GorgeousAndGloreous"),
+                Button.url("DEVELOPER", url="t.me/Anshu888o"),
             ],
         ],
     )
